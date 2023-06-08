9bec7c570313b167d00a2b2c1b06212cd0f23e44
munroev-01918856 <munroev123@gmail.com>
2023-06-07 21:09:19 -0400

    add number input& begin js

--- Script/main.js
+++ Script/main.js
@@ -1,9 +1,33 @@
-function createTable() {
-    var x = document.forms.numberInput.MinX.value;
-    console.log("Nuber");
-    console.log(x);
+var minX;
+var minY;
+var maxX;
+var maxY;
+var error=false;
+var errmsg;
+
+const btn = document.querySelector('.btn');
+
+function setValues() {
+    minX = document.querySelector("#minX");
+    minY = document.querySelector("#minY");
+    maxX = document.querySelector("#maxX");
+    maxY = document.querySelector("#maxY");
+    console.log(maxX.value);
+    validate(minX);
+    validate(minY);
+    validate(maxX);
+    validate(maxY);
+    function validate(userinput) {
+        if((userinput < 1) || (userinput > 50)){
+            error = true;
+            errmsg+="Please input number between 1-50"
+        }
+    }
+    if (error) { console.log(errmsg); }
+}
 
 
-
-
-} 
+btn.addEventListener('click', e => {
+    e.preventDefault();
+    setValues();
+  });
