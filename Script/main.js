55c654f0b4575f256525b63236936217de667361
munroev-01918856 <munroev123@gmail.com>
2023-06-07 21:51:36 -0400

    added validation

--- Script/main.js
+++ Script/main.js
@@ -1,33 +1,57 @@
-var minX;
-var minY;
-var maxX;
-var maxY;
-var error=false;
-var errmsg;
+// File: main.js
+// GUI Assignment: HW 3-Create multiplication table
+// Victoria Munroe victoria_munroe@student.uml.edu
+// Description: Website to take input & return multiplication table
+// This page holds the html scaffoliding
 
+// Copyright (c) 2023 by VMunroe. All rights reserved. May be freely copied or excerpted for educational purposes with credit to the author.
+// created by VM 06/02/23
+
+// Sources: 
+// w3Schools w3schools.com
+// TutorialsPoint 
+// Traversy https://www.youtube.com/watch?v=hdI2bqOjy3c&t=4419s
+ 
+
+
+
+var minXInput= document.querySelector("#minX");
+var minYInput =document.querySelector("#maxX");
+var maxXInput =document.querySelector("#maxX");
+var maxYInput =maxY = document.querySelector("#maxY");
+var error=false;
+var errmsg="None";
+const msg = document.querySelector('.errmsg');
 const btn = document.querySelector('.btn');
+const tableRange= [0, 0, 0, 0];  //min x, min y, max x, max y
 
 function setValues() {
-    minX = document.querySelector("#minX");
-    minY = document.querySelector("#minY");
-    maxX = document.querySelector("#maxX");
-    maxY = document.querySelector("#maxY");
-    console.log(maxX.value);
-    validate(minX);
-    validate(minY);
-    validate(maxX);
-    validate(maxY);
-    function validate(userinput) {
-        if((userinput < 1) || (userinput > 50)){
-            error = true;
-            errmsg+="Please input number between 1-50"
-        }
+    // minY = document.querySelector("#minY");
+    // maxX = document.querySelector("#maxX");
+    // maxY = document.querySelector("#maxY");
+    // console.log(minX);
+    // validate(minX);
+    
+    // if (error){msg.classList.add(errmsg);}
+}
+function validate( num) {
+    if ((num < 0 )||( num > 5)){
+        error = true;
+    errmsg = 'Input number between -50 and 50';
     }
-    if (error) { console.log(errmsg); }
+    // if (error){msg.classList.add(errmsg);}
+    console.log(errmsg);
 }
 
-
+minX.addEventListener('change', e => {
+    e.preventDefault();
+    tableRange[0] = Number(minXInput.value);
+    console.log(tableRange[0]);
+    validate(tableRange[0]);
+});
+  
 btn.addEventListener('click', e => {
     e.preventDefault();
     setValues();
+   
   });
